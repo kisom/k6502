0a0e4a9874e301ce16b8828426ef7016bd9ab9db
Kyle Isom <kyle@tyrfingr.is>
2014-01-28 19:39:16 -0700

    Rename test.cc -> easy6502, update Makefile
    This test isn't an automated test, it involves the user checking the
    output of the register and memory dumps to make sure things are as they
    should be. It's been renamed and easy6502 credited in the source to
    reflect where it comes from and what it's for.
    
    --HG--
    rename : src/test.cc => src/easy6502.cc

--- src/easy6502.cc
+++ src/easy6502.cc
@@ -14,6 +14,9 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
+// This is a test of instructions from http://skilldrick.github.io/easy6502/index.html.
+// As this library isn't an assembler, it won't assemble the files; I'm
+// to check some of the expected behaviours from the emulator.
 
 #include <iostream>
 using namespace std;
@@ -24,6 +27,8 @@ using namespace std;
 void	test1(void);
 void	test2(void);
 void	test3(void);
+void	test4(void);
+void	test5(void);
 
 
 void
@@ -113,10 +118,71 @@ test3()
 }
 
 
+void
+test4()
+{
+	std::cerr << "Starting test 4\n";
+	std::cerr << "\t(branching instruction test)\n";
+	//  LDX #$08
+	//decrement:
+	//  DEX
+	//  STX $0200
+	//  CPX #$03
+	//  BNE decrement
+	//  STX $0201
+	//  BRK
+	CPU	cpu(0x210);
+	uint8_t x = 8;
+
+	cpu.start_pc(0x100);
+	cpu.LDX(0x08);
+	do {
+		cpu.STX(0x200);
+		cpu.DEX();
+		x--;
+		cpu.dump_memory();
+		cpu.dump_registers();
+		cpu.CPX((uint8_t)3);
+		cpu.dump_memory();
+		cpu.dump_registers();
+		cpu.BNE((uint8_t)0xf8);
+	} while (x > 3);
+	cpu.STX(0x201);
+	cpu.BRK();
+	cpu.dump_memory();
+	cpu.dump_registers();
+}
+
+
+void
+test5()
+{
+	std::cerr << "Starting test 5\n";
+	std::cerr << "\t(indirect addressing)\n";
+	//  LDA #$01
+	//  CMP #$02
+	//  BNE notequal
+	//  STA $22
+	//notequal:
+	//  BRK
+	CPU	cpu(0x100);
+
+	cpu.LDA(0x01);
+	cpu.CMP((uint8_t)0x02);
+	cpu.BNE((uint8_t)0x02);
+	// STA skipped
+	cpu.BRK();
+	cpu.dump_memory();
+	cpu.dump_registers();
+}
+
+
 int
 main(void)
 {
 	test1();
 	test2();
 	test3();
+	test4();
+	test5();
 }
